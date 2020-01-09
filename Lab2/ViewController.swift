6f11375839fd0153326e0958be34d8ea77736fd8
Jakub Bogunia <bogunia.jakub@gmail.com>
2020-01-09 12:37:53 -0800

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Jakub Bogunia on 09/01/2020.
+//  Copyright © 2020 Jakub Bogunia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
